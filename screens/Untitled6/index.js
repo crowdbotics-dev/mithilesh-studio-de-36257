30e1ccefba7ee121740a023e310a00a5a6152d0d
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-10-06 10:57:19 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled6 = () => {
-  return <View style={_styles.phNLRwvl}>
+  return <View style={_styles.DFmlSIqp}>
       </View>;
 };
 
 export default Untitled6;
 
 const _styles = StyleSheet.create({
-  phNLRwvl: {
+  DFmlSIqp: {
     backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
